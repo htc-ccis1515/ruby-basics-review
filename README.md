0cc1001aa3482898acf8fd4ca59538b73e231e40
mbMosman <mbmosman@users.noreply.github.com>
2016-11-15 00:27:05 -0600

    initial add

---
+++ README.md
@@ -0,0 +1,11 @@
+# ruby-basics-review
+Practice creating methods and working with lists and iterators in Ruby.  
+
+This lab contains 5 exercises.  Follow the instructions in each comment block to
+complete the assignment.
+
+1. Display a date in the format January 1, 1999
+2. Sum numbers
+3. Determine if the first & last letter of a word are the same
+4. Convert inches to feet
+5. Convert points to a letter grade
---
+++ basics-review.rb
@@ -0,0 +1,78 @@
+##############################################################################
+# Make a method to do a pretty display of a date.  The method should take
+# three input values: the number of the month (1-12), the day and the year.
+# It should return back a string in the format of January 1, 1999.
+#
+# Use an array inside this method to hold the months of the year.
+# Remember that the first position of an array is 0 and the first month (not the
+# 0th) of the year is January.
+#
+# Remember that you must first define the method, then call the method.
+#
+# Setup a short script to:
+# - Ask for each value & convert them to numbers
+# - If the enter a value outside of a reasonable range, show an error message
+#   and ask for the value again.
+# - Pass those 3 values into your method
+# - use the output from the method to display the date
+#############################################################################
+
+
+
+
+##############################################################################
+# Write a method to sum (add up, or total) the numbers from an array.  The method
+# should take in an array of any length (including 0) as input and return back
+# the sum of all the numbers in the array.  If the array is empty, return 0.
+#
+# Test your method, then write the script below.
+#
+# Write a short script to:
+# - Ask the person for 5 numbers (Use a loop to do this, don't duplicate code.)
+# - Add the numbers into an array as they are entered
+# - (Don't forget to convert them to numbers!)
+# - Show the list of numbers that were entered
+# - Use the method you made to sum the numbers
+# - Display the total in a nice message
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method to determine whether or not the first and last letter of a
+# word are the same letter.  Return true from the method if they are and false
+# otherwise.
+#
+# Write a test script for this method that asks a user to enter words until
+# they have entered 'Q' to quit.  After they enter the word, tell them the word
+# they entered, then say if the first and last letter are the same in a nice
+# message.  (Don't just say true or false)
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method convert a number of inches to feet.  Remember that 1 foot is
+# equal to 12 inches.
+#
+# Write a test script for this method that asks a user to enter a number to
+# convert until they have entered 0 to quit.  After they enter the number
+# display a nice message that tells them how many inches they entered and
+# how many feet that converts to.
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method convert a number of points (out of 100) to a letter grade.
+# Use a 10 point scale: 90 and up is an A, 80-90 is a B, etc.  The method should
+# return the letter grade.
+#
+# Write a test script for this method that asks a user to enter a number to
+# convert until they have entered a number less than 0 to quit.  After they
+# enter the number display a nice message that tells them both the number of
+# points and the corresponding letter grade.
+#############################################################################
