1b98591dfd017fd2d69181f8c08bea30fc69d506
Colin Eichhorst <Colin Eichhorst>
2016-11-29 12:17:46 -0600

    added code

--- basics-review.rb
+++ basics-review.rb
@@ -16,8 +16,18 @@
 # - Pass those 3 values into your method
 # - use the output from the method to display the date
 #############################################################################
+def show_date month_of_year, day_of_week
+  month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  day = dom
+  return month[month_of_year-1] + day[day_of_week].to_s
+end
 
-
+puts 'enter a number for the month of the year :'
+num = gets.chomp.to_i
+puts 'enter in a number for day of the month: '
+dom = gets.chomp.to_i
+month = show_date num dom
+puts 'That makes the days date' + month + '.'
 
 
 ##############################################################################
@@ -35,7 +45,26 @@
 # - Use the method you made to sum the numbers
 # - Display the total in a nice message
 #############################################################################
+def sum_numbers list
+  total = 0
+  list.each do |num|
+      total = num + total
+  end
+  return total
+end
 
+numbers = [1,2,3]
+no_numbers = []
+puts "The total of " + numbers.to_s + " is " + sum_numbers(numbers).to_s
+puts "The total of " + no_numbers.to_s + " is " + sum_numbers(no_numbers).to_s
+
+list = []
+while list.length < 5
+  puts "Enter a number: "
+  num = gets.chomp.to_i
+  list.push num
+end
+puts "The total of your number " + list.to_s + " is " + sum_numbers(list).to_s
 
 
 
@@ -49,10 +78,21 @@
 # they entered, then say if the first and last letter are the same in a nice
 # message.  (Don't just say true or false)
 #############################################################################
+def is_first_last_same word
+  return true
+end
 
 
-
-
+word = ''
+while word != '' # Using upcase here makes this check case insesitive both 'Q' and 'q' will stop
+  puts 'enter a word and i\'ll tell you if the first and last letter are the same.'
+  word = gets.chomp
+  if is_first_last_same word
+    puts 'The first and last letters of ' + word + ' are the same.'
+  else
+    puts 'Nope, the first and last letter ' + word + ' are not the same.'
+  end
+end
 ##############################################################################
 # Make a method convert a number of inches to feet.  Remember that 1 foot is
 # equal to 12 inches.
@@ -62,10 +102,15 @@
 # display a nice message that tells them how many inches they entered and
 # how many feet that converts to.
 #############################################################################
+def inches_to_feet inches
+  foot =  inches.to_i / 12
+    return foot
+  end
 
-
-
-
+  puts "Put in a number in inches."
+  inch = gets.chomp
+  foot = inches_to_feet inch
+  puts inch.to_s + " inches is equal to " +  foot.to_s + " feet."
 ##############################################################################
 # Make a method convert a number of points (out of 100) to a letter grade.
 # Use a 10 point scale: 90 and up is an A, 80-90 is a B, etc.  The method should
@@ -76,3 +121,28 @@
 # enter the number display a nice message that tells them both the number of
 # points and the corresponding letter grade.
 #############################################################################
+def letter_grade points
+  grade = 'F'
+  if points >= 90
+    return 'A'
+  elsif points >= 80
+    return 'B'
+    if points >= 70
+      return 'C'
+    elsif points >=60
+      return 'D'
+      if points < 60
+        return 'F'
+      end
+  return grade
+end
+
+points = 100
+while points >= 0
+  puts 'Enter the number of points and I\'ll give you the letter grade.(entering a negative number will stop.)'
+  points = gets.chomp.to_i
+  if points >= 0
+    grade = letter_grade points
+    puts points.to_s + 'is a ' + grade + '.'
+  end
+end
